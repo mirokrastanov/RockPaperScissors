4daa3b6c9a77b3bb3725a1e76c111dc06389fe40
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2022-10-06 11:49:32 +0300

    implement NPC logic

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -17,8 +17,19 @@ function rockPaperScissors(playerTurn) {
         console.log("Invalid Input. Try Again...");
     }
 
-    
+    // Computer logic - turn definition
+    let npcRandomNumber = Math.floor(Math.random() * 3);
+    let npcTurn = "";
+    if (npcRandomNumber == 0) {
+        npcTurn = rock;
+    } else if (npcRandomNumber == 1) {
+        npcTurn = paper;
+    } else if (npcRandomNumber == 2) {
+        npcTurn = scissors;
+    }
+    console.log(`The computer chooses ${npcTurn}`);
 
+   
     
 
 
